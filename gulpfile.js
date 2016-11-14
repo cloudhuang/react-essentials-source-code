99dc4bbab9c8922feb2a26735440346762de6b7d
Liping Huang <liping.huang@live.com>
2016-11-14 12:05:35 +0800

    refactor structure & gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,107 @@
-var gulp = require('gulp');
-var browserify = require('browserify');
-var babelify = require('babelify');
-var source = require('vinyl-source-stream');
-var del = require('del');
+/*!
+ * gulp
+ * $ npm install gulp-ruby-sass gulp-autoprefixer gulp-cssnano gulp-jshint gulp-concat gulp-uglify gulp-imagemin gulp-notify gulp-rename gulp-livereload gulp-cache del --save-dev
+ */
 
-gulp.task('default', [], function() {
-	return browserify('./source/app.js')
-	.transform(babelify, { presets: ['react'] })
-	.bundle()
-	.pipe(source('app.js'))
-	.pipe(gulp.dest('build'))
+// Load plugins
+var gulp = require('gulp'),
+  connect = require('gulp-connect'),
+  sass = require('gulp-ruby-sass'),
+  autoprefixer = require('gulp-autoprefixer'),
+  cssnano = require('gulp-cssnano'),
+  jshint = require('gulp-jshint'),
+  through2 = require('through2'),
+  browserify = require('browserify'),
+  babelify = require('babelify'),
+  source = require('vinyl-source-stream'),
+  uglify = require('gulp-uglify'),
+  imagemin = require('gulp-imagemin'),
+  rename = require('gulp-rename'),
+  concat = require('gulp-concat'),
+  notify = require('gulp-notify'),
+  cache = require('gulp-cache'),
+  livereload = require('gulp-livereload'),
+  del = require('del');
+
+// Styles
+gulp.task('styles', function () {
+  return sass('src/styles/main.scss', { style: 'expanded' })
+    .pipe(autoprefixer('last 2 version'))
+    .pipe(gulp.dest('dist/styles'))
+    .pipe(rename({ suffix: '.min' }))
+    .pipe(cssnano())
+    .pipe(gulp.dest('dist/styles'))
+    .pipe(notify({ message: 'Styles task complete' }));
 });
+
+// Scripts
+gulp.task('scripts', function () {
+  return gulp.src('src/scripts/**/*.js')
+    .pipe(through2.obj(function (file, enc, next) {
+      browserify(file.path)
+        .transform(babelify, { presets: ['react'] })
+        .bundle(function (err, res) {
+          err && console.log(err.stack);
+          file.contents = res;
+          next(null, file);
+        });
+    }))
+    // .pipe(jshint('.jshintrc'))
+    // .pipe(jshint.reporter('default'))
+    .pipe(concat('bundle.js'))
+    .pipe(gulp.dest('dist/scripts'))
+    .pipe(rename({ suffix: '.min' }))
+    .pipe(uglify())
+    .pipe(gulp.dest('dist/scripts'))
+    // .pipe(notify({ message: 'Scripts task complete' }))
+    ;
+});
+
+// Images
+gulp.task('images', function () {
+  return gulp.src('src/images/**/*')
+    .pipe(cache(imagemin({ optimizationLevel: 3, progressive: true, interlaced: true })))
+    .pipe(gulp.dest('dist/images'))
+    .pipe(notify({ message: 'Images task complete' }));
+});
+
+// Clean
+gulp.task('clean', function () {
+  return del(['dist/styles', 'dist/scripts', 'dist/images']);
+});
+
+// Default task
+gulp.task('default', ['clean'], function () {
+  gulp.start('styles', 'scripts', 'images', 'webserver', 'watch');
+});
+
+// Watch
+gulp.task('watch', function () {
+
+  // Watch .scss files
+  gulp.watch('src/styles/**/*.scss', ['styles']);
+
+  // Watch .js files
+  gulp.watch('src/scripts/**/*.js', ['scripts']);
+
+  // Watch image files
+  gulp.watch('src/images/**/*', ['images']);
+
+  // Create LiveReload server
+  livereload.listen();
+
+  // Watch any files in dist/, reload on change
+  gulp.watch(['dist/**']).on('change', livereload.changed);
+
+});
+
+// Web server
+gulp.task('webserver', function() {
+  connect.server({
+    port: 3000,
+    livereload: true,
+    root: ['dist', '.tmp']
+  });
+});
+
+// https://code.tutsplus.com/tutorials/gulp-as-a-development-web-server--cms-20903
--- src/scripts/app.js
+++ src/scripts/app.js
@@ -3,7 +3,13 @@ var ReactDOM = require('react-dom')
 
 var ReactClass = React.createClass({
 	render: function() {
-		return React.createElement('h1', { className: 'header' }, 'React Component');
+
+		if (this.props.isHidden) {
+			return null;
+		}
+
+		var header = this.props.header + ' Lastest Tweets';
+		return React.createElement('h1', { className: 'header' }, header);
 	}
 });
 
