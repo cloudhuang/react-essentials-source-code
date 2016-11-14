d0d963f82faad5a527e9b04bf6d90af1f5c1ac7c
Liping Huang <liping.huang@live.com>
2016-11-14 13:02:50 +0800

    add React lifecycle methods

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -2,15 +2,53 @@ var React = require('react')
 var ReactDOM = require('react-dom')
 
 var ReactClass = React.createClass({
+	getInitialState: function() {
+		console.log('getInitialState');
+		return {
+			header: 'Hello World'
+		};
+	},
+	
+	componentWillMount: function() {
+		console.log('componentWillMount');
+	},
+
+	componentDidMount: function() {
+		console.log('componentDidMount');
+	},
+
+	componentWillUnmount: function() {
+		console.log('componentWillUnmount');
+	},
+
+	componentWillReceiveProps: function(nextProps) {
+		console.log('componentWillReceiveProps: ' + nextProps );
+	},
+
+	shouldComponentUpdate: function() {
+		console.log('shouldComponentUpdate');
+	},
+
+	componentWillUpdate: function() {
+		console.log('componentWillUpdate');
+	},
+
 	render: function() {
+		console.log('render');
 
 		if (this.props.isHidden) {
 			return null;
 		}
 
-		var header = this.props.header + ' Lastest Tweets';
+		var header = this.state.header + ' Lastest Tweets';
 		return React.createElement('h1', { className: 'header' }, header);
+	},
+
+	componentDidUpdate: function() {
+		console.log('componentDidUpdate');
 	}
+
+
 });
 
 var reactComponentElement = React.createElement(ReactClass);
